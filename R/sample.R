ed7eaa0aef06eb0252b24f5a3a30eeec18d61f3a
Jake Kaupp <jake.kaupp@queensu.ca>
2019-10-14 21:04:29 -0400

    added tidyr to imports

--- R/sample.R
+++ R/sample.R
@@ -1,15 +1,14 @@
 #' Retrieve file paths to TCPS sample data.
 #'
 #' tcps comes bundled with some example files in its `inst/extdata`
-#' directory. This function make them easy to access.
+#' directory. This function make them easy to access by passing the sample name
+#' as a quoted string.  It can be one of "staff_sample.xlsx", "student_sample.xlsx" or
+#' "faculty_sample.xlsx"
 #'
 #' @param path Name of file. If `NULL`, the example files will be listed.
 #' @param ... Additional parameters pass on to dir
 #'
 #' @export
-#' @examples
-#' tcps_sample()
-#' tcps_sample("student_sample.xlsx")
 tcps_sample <- function(path = NULL, ...) {
   if (is.null(path)) {
     dir(system.file("extdata", package = "tcps"), ...)
